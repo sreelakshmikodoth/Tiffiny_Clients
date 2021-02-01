000de6f4eb316a92eaaa7f014bf9fb73a86d9bbd
sree <me.kodoth@hotmail.com>
2021-01-31 23:43:35 -0800

    dd

--- src/util/axios.js
+++ src/util/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_SERVER_URL,
+  baseURL: "https://tiffinyserverapp.herokuapp.com/",
 });
 
 export default axiosInstance;
